8707683c28e8cfc492c50d3c13a0b698f9a7f167
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-07-28 11:18:39 +0900

    Delete BackEnd directory

--- BackEnd/application/utils/SessionUtils.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function getLoginUser() {
-        return isset($_SESSION[_LOGINUSER]) ? $_SESSION[_LOGINUSER] : null;
-    }
-
-    function getIuser() {
-        return getLoginUser() === null ? 0 : getLoginUser()->iuser;
-    }
