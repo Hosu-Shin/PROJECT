6499c12078c135591cadd8ddc5a73d5c4075ef8c
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-07-28 17:21:11 +0900

    [한 끼 어때] 군/구 붙이기

--- 한 끼 어때/FrontEnd/src/views/HomeView.vue
+++ 한 끼 어때/FrontEnd/src/views/HomeView.vue
@@ -55,7 +55,12 @@ export default {
             let Addr3 = Addr[i].area3
             
               this.RestArea.push(Addr1);
+
+            if(Addr[i].area2 !== '' && Addr[i].area3 !== '') {
+              this.SubArea.push(Addr2+'/'+Addr3);
+            } else {
               this.SubArea.push(Addr2,Addr3);
+            }
 
         }
         this.RestArea = Array.from(new Set(this.RestArea))
