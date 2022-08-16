35e140260c710c6cbda987824ca18081eb5d320f
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-08-16 18:04:26 +0900

    Add files via upload

--- 한 끼 어때/BackEnd/application/models/SearchModel.php
+++ 한 끼 어때/BackEnd/application/models/SearchModel.php
@@ -5,11 +5,12 @@
     class SearchModel extends Model {
         //메뉴코드 가져오기
         public function getMenuCD(&$param) {
+            $menucd = $param["search_word"];
             $sql = "SELECT imcd FROM menu_cd 
-                    WHERE menucd = :menucd";
+                    WHERE '$menucd' LIKE concat('%', menucd, '%')";
     
             $stmt = $this->pdo->prepare($sql);
-            $stmt->bindValue(":menucd", $param["search_word"]);
+            // $stmt->bindValue(":menucd", "%".$param["search_word"]."%");
             $stmt->execute();
 
             $rs = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -18,33 +19,53 @@
 
         //검색 시 음식점 리스트 가져오기
         public function getRestList(&$param) {
-            $sql = "SELECT a.*, c.menucd
-                    FROM restaurant a, menu_list b, menu_cd c
-                    WHERE a.irest = b.irest AND b.imcd = c.imcd AND c.menucd LIKE :menucd
+            $iuser = $param["iuser"];
+            $menucd = $param["search_word"];
+            $lon_x = $param["lon_x"];
+            $lat_y = $param["lat_y"];
+            $sql = "SELECT a.*,
+                        (6371*ACOS(COS(RADIANS($lat_y))*COS(RADIANS(a.lat_y))*COS(RADIANS(a.lon_x)-RADIANS($lon_x))+SIN(RADIANS($lat_y))*SIN(RADIANS(a.lat_y)))) AS dis, 
+                        c.menucd, z.iuser, z.rating
+                    FROM restaurant a
+                    INNER JOIN menu_list b
+                    ON a.irest = b.irest
+                    INNER JOIN menu_cd c
+                    ON b.imcd = c.imcd
+                    LEFT JOIN ( SELECT d.irest, d.iuser, d.rating FROM user_diary d
+                                INNER JOIN user s
+                                ON d.iuser = s.iuser
+                                WHERE d.iuser = {$iuser} ) as z
+                    ON a.irest = z.irest
+                    WHERE a.irest = b.irest AND b.imcd = c.imcd AND '$menucd' LIKE concat('%', menucd, '%')
                     GROUP BY a.irest
-                    ORDER BY a.irest asc";
+                    HAVING dis <= 6
+                    ORDER BY dis";
             
             $stmt = $this->pdo->prepare($sql);
-            $stmt->bindValue(":menucd", "%".$param["search_word"]."%");
+            // $stmt->bindValue(":menucd", "%".$param["search_word"]."%");
+            // $stmt->bindValue(":iuser", $iuser);
             $stmt->execute();
 
             return $stmt->fetchAll(PDO::FETCH_OBJ); 
 
-            //(6371*ACOS(COS(RADIANS(:lat_y))*COS(RADIANS(a.lat_y))*COS(RADIANS(a.lon_x)-RADIANS(:lon_x))+SIN(RADIANS(:lat_y))*SIN(RADIANS(a.lat_y)))) AS dis
+            //
             //$stmt->bindValue(":lat_y", $param["lat_y"]);
             //$stmt->bindValue(":lon_x", $param["lon_x"]);
-            //HAVING dis <= 1
-            //ORDER BY dis
+            //
+            //ORDER BY 
         }
 
         //검색 로그 저장
         public function insSearchLog(&$param) {
+            $iuser = $param["iuser"];
             $sql = "INSERT INTO search_log (search_word, iuser)
                     VALUES (:search_word, :iuser)";
-            
+            if(empty($iuser)) {
+                $iuser = 0;
+            }
             $stmt = $this->pdo->prepare($sql);
             $stmt->bindValue(":search_word", $param["search_word"]);
-            $stmt->bindValue(":iuser", $param["iuser"]);
+            $stmt->bindValue(":iuser", $iuser);
             $stmt->execute();
 
             return $stmt->rowCount();
@@ -54,13 +75,14 @@
         public function getMostSearchLog() {
             $sql = "SELECT search_word
                     FROM search_log
+                    WHERE DATE(search_date) = DATE(NOW())
                     GROUP BY search_word
                     HAVING COUNT(*) = ( SELECT MAX(mycnt)
                                         FROM ( 
-                                            SELECT search_word, COUNT(*) AS mycnt
-                                            FROM search_log
-                                            WHERE DATE(search_date) = DATE(NOW())
-                                            GROUP BY search_word) AS rs )";
+                                        SELECT search_word, COUNT(*) AS mycnt
+                                        FROM search_log
+                                        WHERE DATE(search_date) = DATE(NOW())
+                                        GROUP BY search_word) AS rs )";
             
             $stmt = $this->pdo->prepare($sql);
             $stmt->execute();
@@ -126,5 +148,18 @@
             $stmt->execute();
 
             return $stmt->fetchAll(PDO::FETCH_OBJ); 
+        }
+
+        //메뉴 가져오기
+        public function selMenuList(&$param) {
+            $sql = "SELECT irest FROM menu_list
+                    WHERE irest = :irest 
+                    GROUP BY irest";
+
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindValue(":irest", $param["irest"]);
+            $stmt->execute();
+
+            return $stmt->fetch(PDO::FETCH_OBJ); 
         }
     }
--- 한 끼 어때/BackEnd/application/models/UserModel.php
+++ 한 끼 어때/BackEnd/application/models/UserModel.php
@@ -44,6 +44,31 @@ class UserModel extends Model {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
+    //비밀번호 찾기위해 이메일 있는지 체크
+    public function checkEmail(&$param) {
+        $sql = " SELECT COUNT(*) as cnt FROM user WHERE email = :email and social_type = 0 ";
+        
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':email', $param["email"]);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    //재설정된 비번 insert
+    public function updPassword(&$param) {
+        $sql = "UPDATE user
+                SET pw = :pw
+                WHERE email = :email and social_type = 0";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':email', $param["email"]);
+        $stmt->bindValue(':pw', $param["pw"]);
+        $stmt->execute();
+
+        return $stmt->rowCount();
+    }
+
     public function updateUser(&$param) {
         $sql = "UPDATE user SET ";
         if(isset($param['nick']) && isset($param['birth']) && isset($param['job'])){
