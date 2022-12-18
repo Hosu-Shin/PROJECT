b0899efc36cce6aab86244abcc85604debe64980
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-12-18 16:05:31 +0900

    [ 한 끼 어때 ] BackEnd_Node
    최종본 백업

--- 한 끼 어때/nodeback/public/server.js
+++ 한 끼 어때/nodeback/public/server.js
@@ -4,12 +4,11 @@ const http = require('http').createServer(app);
 const port = process.env.PORT || 3000;
 const path = require("path");
 
-//setting cors 
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
-});
+})
 
 app.use(express.static('public'));
 
@@ -17,22 +16,3 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
-
-
-const server = http.listen(port, () => {
-    console.log(`server is listening at localhost:${port}`);
-});
-
-const socketIO = require('socket.io');
-
-const io = socketIO(server, { path: '/socket.io'});
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('msg', (msg) => {
-      io.emit('msg', msg);
-    });
-    socket.on('disconnect', reason => {
-        console.log(reason);
-        console.log('user disconnected');
-    });
-  });
